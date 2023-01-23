94d31a17a30c2595d881c4f42ab93fbaa534af55
Sjoerd <sjoerdschot@gmail.com>
2023-01-23 14:28:12 +0100

    Clown script animation fix

--- Assets/Scripts/ClownActivate.cs
+++ Assets/Scripts/ClownActivate.cs
@@ -15,7 +15,7 @@ public class ClownActivate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = gameObject.GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
